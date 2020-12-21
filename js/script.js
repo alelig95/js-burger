67508dd14df435d0f5c0d173f0a78eaaf2ba443a
Alessandro Liggieri <smmb@live.it>
2020-12-21 13:10:31 +0100

    Impostazione preliminare esercizio

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Il primo problema è che dobbiamo far apparire un alert quando non viene scelto nessun nome del Burger;
+// Il secondo problema è far apparire un alert se l'utente seleziona meno di due ingredienti;
+// Il terzo problema è calcolare il prezzo finale del panino a seconda degli ingredienti scelti;
+// Il quarto problema è applicare il 20% di sconto se l'utente digita uno dei coupon ideonei per ricevere lo sconto;
