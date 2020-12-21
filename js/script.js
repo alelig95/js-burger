c7d44dda28c6e6cba164ed63c1664ab1c54ed0f8
Alessandro Liggieri <smmb@live.it>
2020-12-21 16:38:00 +0100

    Prezzo finale senza sconto calcolato

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 // Il secondo problema è far apparire un alert se l'utente seleziona meno di due ingredienti;
 // Il terzo problema è calcolare il prezzo finale del panino a seconda degli ingredienti scelti;
 // Il quarto problema è applicare il 20% di sconto se l'utente digita uno dei coupon ideonei per ricevere lo sconto;
+var costoIngrediente = document.getElementsByClassName('ingredient-container')[0].getElementsByTagName('input');
+costoTotale = 0;
+var costoTotaleHTML = document.getElementById('price-output');
+var listaCoupon = ['1HRPLBNR', '1CSUBNJG', '1DRTBNHP', '1TXCJGTR'];
+var inputCoupon = document.getElementsByClassName('coupon')[0];
+var sconto = (costoTotale * 20) / 100;
+
+document.getElementById("pulsante-calcolo").addEventListener("click", function() {
+
+      costoTotale = 0;
+
+
+
+      for(var i = 0; i < costoIngrediente.length; i++) {
+        if(costoIngrediente[i].checked) {
+          costoTotale += parseInt(costoIngrediente[i].value);
+        }
+      }
+
+      if(inputCoupon === listaCoupon[i]) {
+        costoTotale = costoTotale - sconto;
+      }
+      // popolo elemento #total con il totale del mio ordine
+
+      costoTotaleHTML.innerText = costoTotale;
+    });
