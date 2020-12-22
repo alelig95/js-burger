c024381687a68b4ae64ce11a5e38701306470c06
Alessandro Liggieri <smmb@live.it>
2020-12-22 13:06:23 +0100

    Lavorando sul css

--- js/script.js
+++ js/script.js
@@ -3,23 +3,37 @@
 // Il terzo problema è calcolare il prezzo finale del panino a seconda degli ingredienti scelti;
 // Il quarto problema è applicare il 20% di sconto se l'utente digita uno dei coupon ideonei per ricevere lo sconto;
 var costoIngrediente = document.getElementsByClassName('ingredient-container')[0].getElementsByTagName('input');
-costoTotale = 0;
+var nomePanino = document.getElementById('name-burger');
+var costoBase = 3;
+var numeroMinimoIngredienti = 2;
 var costoTotaleHTML = document.getElementById('price-output');
 var listaCoupon = ['1HRPLBNR', '1CSUBNJG', '1DRTBNHP', '1TXCJGTR'];
 var inputCoupon = document.getElementsByClassName('coupon')[0];
+var ingredientiSelezionati = 0;
+var sconto = (costoTotale * 20) / 100;
+var costoTotale;
 
 
 document.getElementById("pulsante-calcolo").addEventListener("click", function() {
 
-      costoTotale = 0;
+      costoTotale = costoBase;
+      ingredientiSelezionati = 0;
+
 
       for(var i = 0; i < costoIngrediente.length; i++) {
         if(costoIngrediente[i].checked) {
+          ingredientiSelezionati++;
           costoTotale += parseInt(costoIngrediente[i].value);
         }
       }
 
-      var sconto = (costoTotale * 20) / 100;
+      if(ingredientiSelezionati < numeroMinimoIngredienti) {
+        alert('Seleziona almeno ' + numeroMinimoIngredienti + ' ingredienti');
+    } else if(nomePanino.value.length === 0) {
+        alert('Il tuo panino deve avere un nome!');
+    } else {
+
+
 
       for (var i = 0; i < listaCoupon.length; i++) {
         if(inputCoupon.value === listaCoupon[i]) {
@@ -27,7 +41,7 @@ document.getElementById("pulsante-calcolo").addEventListener("click", function()
         }
       }
 
-      costoTotaleHTML.innerText = costoTotale;
+      costoTotaleHTML.innerText = '$ ' + costoTotale;
 
-
-    });
+      }
+  });
